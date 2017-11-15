f07125ef46d77b03c7908e40fce5bb4cf2af95e3
Dan Richards <dan.c.richards@gmail.com>
2017-11-16 10:59:01 +1300

    Added more pretty stuff

--- ascii.py
+++ ascii.py
@@ -1,6 +1,5 @@
 import random
 import os
-i = random.randrange(10)
-i = 1 #Putting to one at the moment, as there is only 1 art file on the page
+i = random.randrange(9)
 website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
