a791ff372281fc15eb452a53df45cfcb3de26235
Dan Richards <dan.c.richards@gmail.com>
2017-11-16 10:33:57 +1300

    Completed run file

--- ascii.py
+++ ascii.py
@@ -1,4 +1,6 @@
 import random
-
+import os
 i = random.randrange(10)
-prefix = 
+i = 1 #Putting to one at the moment, as there is only 1 art file on the page
+website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
+os.system("curl " +  website)
