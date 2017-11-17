6327077cebbe29673691e6a749a580700fa3bf6c
Dan Richards <dan.c.richards@gmail.com>
2017-11-17 23:38:09 +1300

    Computers make good ascii art

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
-i = random.randrange(8) + 1 
+i = random.randrange(9) + 1 
 website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
